2c62a400fbd0379112166c1c38846014c3090a0a
MALLA NAGA VENKATA PRASANTH NAIDU <110344132+prasanthmalla77@users.noreply.github.com>
2022-12-26 20:45:54 +0530

    Update list_example.py

--- list_example.py
+++ list_example.py
@@ -1,1 +1,13 @@
+def arrayIndex(n,arr,a):
+    if a not in arr :
+        return -1
+    else:
+        for i in range(n):
+            if arr[i]==a:
+                return i
 
+n = int(input())
+arr = [int(x) for x in input().split()]
+a = int(input())
+
+print(arrayIndex(n,arr,a))
